02519cd75885b1bd6c57d1940496c6441604267b
Devesh Ruttala <45329115+deveshruttala@users.noreply.github.com>
2025-04-11 01:22:36 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,5 @@
-
 <?php
-//change mysqli_connect(host_name,username, password); 
+// Change mysqli_connect(host_name, username, password);
 $connection = mysqli_connect("db", "phpmyadmin", "phpmyadmin");
 $db = mysqli_select_db($connection, 'demo');
 ?>
